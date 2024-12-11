f09c701c2491e35f852b9f655acb74d7f2f66af0
Ahmad <muhammadahmadamjad0@gmail.com>
2024-12-11 21:53:00 +0500

    Adjusted Trees and Graphs

--- data_structures/Graph.h
+++ data_structures/Graph.h
@@ -2,42 +2,43 @@
 #define GRAPH_H
 
 #include <iostream>
-#include <unordered_map>
-#include <string>
-#include <queue>
+#include "Queue.h"
 using namespace std;
 
-template <typename T>
 struct Node_graph {
-    T val;
+    int val;
     Node_graph* next;
-    Node_graph(T value) : val(value), next(nullptr) {}
+    Node_graph(int value) : val(value), next(nullptr) {}
 };
 
-template <typename T>
 class Graph {
 private:
-    unordered_map<string, Node_graph<T>*> list; // Keyed by a unique string identifier (e.g., name)
+    Node_graph** adjacencyList;
+    int size;
 
 public:
-    Graph() {}
+    Graph(int vertices) {
+        size = vertices;
+        adjacencyList = new Node_graph*[size];
+        for (int i = 0; i < size; ++i) {
+            adjacencyList[i] = nullptr;
+        }
+    }
 
-    void addEdge(const string& uKey, T uVal, const string& vKey, T vVal) {
-        // Add edge from u to v
-        Node_graph<T>* newNode = new Node_graph<T>(vVal);
-        newNode->next = list[uKey];
-        list[uKey] = newNode;
+    void addEdge(int u, int v) {
+        Node_graph* newNode = new Node_graph(v);
+        newNode->next = adjacencyList[u];
+        adjacencyList[u] = newNode;
 
-        // Add edge from v to u (undirected graph)
-        newNode = new Node_graph<T>(uVal);
-        newNode->next = list[vKey];
-        list[vKey] = newNode;
+        newNode = new Node_graph(u);
+        newNode->next = adjacencyList[v];
+        adjacencyList[v] = newNode;
     }
 
     void printGraph() const {
-        for (const auto& pair : list) {
-            cout << "Vertex " << pair.first << ": ";
-            Node_graph<T>* temp = pair.second;
+        for (int i = 0; i < size; ++i) {
+            cout << "Vertex " << i << ": ";
+            Node_graph* temp = adjacencyList[i];
             while (temp) {
                 cout << temp->val << " -> ";
                 temp = temp->next;
@@ -46,62 +47,64 @@ public:
         }
     }
 
-    void BFS(const string& startKey) {
-        unordered_map<string, bool> visited;
-        queue<string> q;
+    void BFS(int start) {
+        bool* visited = new bool[size]();
+        Queue<int> q;
 
-        visited[startKey] = true;
-        q.push(startKey);
+        visited[start] = true;
+        q.enqueue(start);
 
-        cout << "BFS starting from vertex " << startKey << ": ";
+        cout << "BFS starting from vertex " << start << ": ";
 
-        while (!q.empty()) {
-            string currentKey = q.front();
-            q.pop();
-            cout << currentKey << " ";
+        while (!q.isEmpty()) {
+            int vertex = q.dequeue();
+            cout << vertex << " ";
 
-            Node_graph<T>* temp = list[currentKey];
+            Node_graph* temp = adjacencyList[vertex];
             while (temp) {
-                string adjKey = temp->val.getName(); // Assuming T has a `getName` function
-                if (!visited[adjKey]) {
-                    visited[adjKey] = true;
-                    q.push(adjKey);
+                int adjVertex = temp->val;
+                if (!visited[adjVertex]) {
+                    visited[adjVertex] = true;
+                    q.enqueue(adjVertex);
                 }
                 temp = temp->next;
             }
         }
         cout << endl;
+        delete[] visited;
     }
 
-    void DFS(const string& startKey) {
-        unordered_map<string, bool> visited;
-        DFSHelper(startKey, visited);
+    void DFS(int start) {
+        bool* visited = new bool[size]();
+        DFSHelper(start, visited);
         cout << endl;
+        delete[] visited;
     }
 
-    void DFSHelper(const string& key, unordered_map<string, bool>& visited) {
-        visited[key] = true;
-        cout << key << " ";
+    void DFSHelper(int vertex, bool* visited) {
+        visited[vertex] = true;
+        cout << vertex << " ";
 
-        Node_graph<T>* temp = list[key];
+        Node_graph* temp = adjacencyList[vertex];
         while (temp) {
-            string adjKey = temp->val.getName(); // Assuming T has a `getName` function
-            if (!visited[adjKey]) {
-                DFSHelper(adjKey, visited);
+            int adjVertex = temp->val;
+            if (!visited[adjVertex]) {
+                DFSHelper(adjVertex, visited);
             }
             temp = temp->next;
         }
     }
 
     ~Graph() {
-        for (auto& pair : list) {
-            Node_graph<T>* temp = pair.second;
+        for (int i = 0; i < size; ++i) {
+            Node_graph* temp = adjacencyList[i];
             while (temp) {
-                Node_graph<T>* toDelete = temp;
+                Node_graph* toDelete = temp;
                 temp = temp->next;
                 delete toDelete;
             }
         }
+        delete[] adjacencyList;
     }
 };
 
--- data_structures/avl_tree.h
+++ data_structures/avl_tree.h
@@ -2,43 +2,36 @@
 #define AVLTREE_H
 
 #include <iostream>
-#include <algorithm> 
+#include <algorithm>
 
 using namespace std;
 
 template <typename T>
-struct Node_tree 
-{
-    T val;
+struct Node_tree {
+    T obj; // The object storing details
+    int key;
     Node_tree* left;
     Node_tree* right;
     int height;
 
-    Node_tree(T value) {
-        val = value;
-        left = right = nullptr;
-        height = 1;
-    }
+    Node_tree(T object, int k) : obj(object), key(k), left(nullptr), right(nullptr), height(1) {}
 };
 
+
 template <typename T>
-class AVL 
-{
+class AVL {
 private:
     Node_tree<T>* root;
 
-    int getHeight(Node_tree<T>* node) 
-    {
+    int getHeight(Node_tree<T>* node) {
         return node ? node->height : 0;
     }
 
-    int getBalanceFactor(Node_tree<T>* node) 
-    {
+    int getBalanceFactor(Node_tree<T>* node) {
         return node ? getHeight(node->left) - getHeight(node->right) : 0;
     }
 
-    Node_tree<T>* leftRotate(Node_tree<T>* x) 
-    {
+    Node_tree<T>* leftRotate(Node_tree<T>* x) {
         Node_tree<T>* y = x->right;
         Node_tree<T>* T2 = y->left;
 
@@ -51,8 +44,7 @@ private:
         return y;
     }
 
-    Node_tree<T>* rightRotate(Node_tree<T>* y) 
-    {
+    Node_tree<T>* rightRotate(Node_tree<T>* y) {
         Node_tree<T>* x = y->left;
         Node_tree<T>* T2 = x->right;
 
@@ -65,38 +57,30 @@ private:
         return x;
     }
 
-    Node_tree<T>* add(Node_tree<T>* node, T val) 
-    {
-        if (!node) return new Node_tree<T>(val);
+    Node_tree<T>* add(Node_tree<T>* node, T val, int key) {
+        if (!node) return new Node_tree<T>(val, key);
 
-        if (val < node->val) 
-        {
-            node->left = add(node->left, val);
-        } 
-        else if (val > node->val) 
-        {
-            node->right = add(node->right, val);
+        if (key < node->key) {
+            node->left = add(node->left, val, key);
+        } else if (key > node->key) {
+            node->right = add(node->right, val, key);
         }
 
         node->height = 1 + max(getHeight(node->left), getHeight(node->right));
 
         int balance = getBalanceFactor(node);
 
-        if (balance > 1 && val < node->left->val) 
-        {
+        if (balance > 1 && key < node->left->key) {
             return rightRotate(node);
         }
-        if (balance > 1 && val > node->left->val) 
-        {
+        if (balance > 1 && key > node->left->key) {
             node->left = leftRotate(node->left);
             return rightRotate(node);
         }
-        if (balance < -1 && val > node->right->val) 
-        {
+        if (balance < -1 && key > node->right->key) {
             return leftRotate(node);
         }
-        if (balance < -1 && val < node->right->val) 
-        {
+        if (balance < -1 && key < node->right->key) {
             node->right = rightRotate(node->right);
             return leftRotate(node);
         }
@@ -104,46 +88,23 @@ private:
         return node;
     }
 
-    void preOrder(Node_tree<T>* node) 
-    {
+    void preOrder(Node_tree<T>* node) {
         if (!node) return;
-        cout << node->val << " ";
+        node->obj.print_details(); // Call the object's function
         preOrder(node->left);
         preOrder(node->right);
     }
 
-    void inOrder(Node_tree<T>* node) 
-    {
-        if (!node) return;
-        inOrder(node->left);
-        cout << node->val << " ";
-        inOrder(node->right);
-    }
 
-    void postOrder(Node_tree<T>* node) 
-    {
-        if (!node) return;
-        postOrder(node->left);
-        postOrder(node->right);
-        cout << node->val << " ";
-    }
-
-    Node_tree<T>* deleteNode(Node_tree<T>* node, T val) 
-    {
+    Node_tree<T>* deleteNode(Node_tree<T>* node, int key) {
         if (!node) return node;
 
-        if (val < node->val) 
-        {
-            node->left = deleteNode(node->left, val);
-        } 
-        else if (val > node->val) 
-        {
-            node->right = deleteNode(node->right, val);
-        } 
-        else 
-        {
-            if (!node->left || !node->right) 
-            {
+        if (key < node->key) {
+            node->left = deleteNode(node->left, key);
+        } else if (key > node->key) {
+            node->right = deleteNode(node->right, key);
+        } else {
+            if (!node->left || !node->right) {
                 Node_tree<T>* temp = node->left ? node->left : node->right;
                 delete node;
                 return temp;
@@ -151,27 +112,24 @@ private:
 
             Node_tree<T>* temp = inOrderPredecessor(node);
             node->val = temp->val;
-            node->left = deleteNode(node->left, temp->val);
+            node->key = temp->key;
+            node->left = deleteNode(node->left, temp->key);
         }
 
         node->height = 1 + max(getHeight(node->left), getHeight(node->right));
         int balance = getBalanceFactor(node);
 
-        if (balance > 1 && val < node->left->val) 
-        {
+        if (balance > 1 && getBalanceFactor(node->left) >= 0) {
             return rightRotate(node);
         }
-        if (balance > 1 && val > node->left->val) 
-        {
+        if (balance > 1 && getBalanceFactor(node->left) < 0) {
             node->left = leftRotate(node->left);
             return rightRotate(node);
         }
-        if (balance < -1 && val > node->right->val) 
-        {
+        if (balance < -1 && getBalanceFactor(node->right) <= 0) {
             return leftRotate(node);
         }
-        if (balance < -1 && val < node->right->val) 
-        {
+        if (balance < -1 && getBalanceFactor(node->right) > 0) {
             node->right = rightRotate(node->right);
             return leftRotate(node);
         }
@@ -179,8 +137,7 @@ private:
         return node;
     }
 
-    Node_tree<T>* inOrderPredecessor(Node_tree<T>* node) 
-    {
+    Node_tree<T>* inOrderPredecessor(Node_tree<T>* node) {
         node = node->left;
         while (node->right) {
             node = node->right;
@@ -189,33 +146,19 @@ private:
     }
 
 public:
+
     AVL() : root(nullptr) {}
 
-    void insert(T val) 
-    {
-        root = add(root, val);
+    void insert(T val, int key) {
+        root = add(root, val, key);
     }
 
-    void traverseTree(int key) 
-    {
-        switch (key) {
-        case 0:
-            preOrder(root);
-            break;
-        case 1:
-            postOrder(root);
-            break;
-        case 2:
-            inOrder(root);
-            break;
-        default:
-            break;
-        }
+    void traverseTree() {
+        preOrder(root);
     }
 
-    void deletion(T val) 
-    {
-        root = deleteNode(root, val);
+    void deletion(int key) {
+        root = deleteNode(root, key);
     }
 };
 
